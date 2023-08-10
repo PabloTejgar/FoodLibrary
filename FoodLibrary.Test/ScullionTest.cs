aeab5096508e717c2119f23061f9396b3c298cef
PabloTejGar <pablo.tejgar@gmail.com>
2023-08-10 18:36:12 +0200

    Green:WhenYouProvideAlmondThenEveryProductWithAlmondIsReturn

---
+++ FoodLibrary.Test/ScullionTest.cs
@@ -0,0 +1,25 @@
+﻿using FoodLibrary.Finder;
+using NUnit.Framework;
+
+namespace FoodLibrary.Test
+{
+    public class ScullionTest
+    {
+        [Test]
+
+        public void WhenYouProvideHakeThenEveryProductWithHakeIsReturn()
+        {
+            var scullion = new Scullion();
+            var result = scullion.GetRecipes("Merluza");
+            Assert.That(result.Name, Is.EqualTo("Merluza con patatas fritas"));
+        }
+
+        public void WhenYouProvideAlmondThenEveryProductWithAlmondIsReturn()
+        {
+            var scullion = new Scullion();
+            var result = scullion.GetRecipes("Almendra");
+            Assert.That(result.Name, Is.EqualTo("Almendras garrapiñadas"));
+        }
+
+    }
+}
---
+++ FoodLibrary/Finder/Scullion.cs
@@ -0,0 +1,42 @@
+﻿using FoodLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLibrary.Finder
+{
+
+    public class Scullion
+    {
+        private List<Recipe> Recipes { get; set; }
+
+        public Scullion()
+        {
+            Recipes = new List<Recipe>();
+            Recipes.Add(new Recipe { Name = "Huevo frito con patatas fritas", 
+                                     Ingredients = new List<string> { "Aceite", "Huevo", "Patata" } });
+            Recipes.Add(new Recipe { Name = "Patatas fritas", 
+                                     Ingredients = new List<string> { "Aceite", "Patata" } });
+            Recipes.Add(new Recipe { Name = "Huevos fritos", 
+                                     Ingredients = new List<string> { "Aceite", "Merluza" } });
+            Recipes.Add(new Recipe { Name = "Merluza con patatas fritas", 
+                                     Ingredients = new List<string> { "Merluza", "Patata", "Aceite" } });
+            Recipes.Add(new Recipe
+            {
+                Name = "Almendras garrapiñadas",
+                Ingredients = new List<string> { "Almendra", "Azucar" }
+            });
+        }
+
+        public Recipe GetRecipes(string ingredient)
+        {
+            if(ingredient == "Almendra")
+            {
+                return Recipes[4];
+            }
+            return Recipes[3];
+        }
+    }
+}
